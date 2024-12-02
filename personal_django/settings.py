aeab01226eeb7ecebfee1584ffc3730c8790198d
Friday G <faradayyg@gmail.com>
2024-12-02 20:24:08 +0100

    add gunicorn

--- personal_django/settings.py
+++ personal_django/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-7mctxecz18tp)xmbytq#9)c9$b7plr2c9m@lj3z)&esd=a3%9!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
